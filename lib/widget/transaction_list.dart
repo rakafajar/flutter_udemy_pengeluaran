0894520d7e29c0cfe8fc8e5e879c1024fb6951d7
raka <rakafajar18@gmail.com>
2020-02-09 22:27:06 +0700

    transaction_list.dart, changelog : Change Column to ListView.Builder

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -10,60 +10,56 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transaction.map(
-            (tx) {
-              return Card(
-                elevation: 10,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      child: Text(
-                        'Rp. ${tx.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                          fontSize: 20,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
+      height: 300,
+      child: ListView.builder(
+        itemCount: transaction.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 10,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          // Class untuk membuat format data
-                          // menggunakan paket library
-                          DateFormat('dd MMMM y').format(tx.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                  ),
+                  child: Text(
+                    'Rp. ${transaction[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
+                      fontSize: 20,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
                 ),
-              );
-            },
-          ).toList(),
-        ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transaction[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      // Class untuk membuat format data
+                      // menggunakan paket library
+                      DateFormat('dd MMMM y').format(transaction[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
