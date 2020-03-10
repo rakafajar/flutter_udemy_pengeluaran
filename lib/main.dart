991a5cabd1d9a05661a3b03362953e2a92ee957f
raka <rakafajar18@gmail.com>
2020-03-10 20:30:28 +0700

    Configurasi Theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
