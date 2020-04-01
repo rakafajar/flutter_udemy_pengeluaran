903261c1d42f7dec9d48a1dee85c93f30f73a3d1
raka <rakafajar18@gmail.com>
2020-04-01 23:31:21 +0700

    add chart tabel

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_pengeluaran/models/transaction.dart';
+import 'package:udemy_pengeluaran/widgets/chart.dart';
 import 'package:udemy_pengeluaran/widgets/transaction_list.dart';
 import './widgets/new_transaction.dart';
 
@@ -59,6 +60,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactoions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       title: txTitle,
@@ -102,15 +113,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
-            TransactionList(_userTransactions)
+            Chart(_recentTransactoions),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:udemy_pengeluaran/models/transaction.dart';
+import 'package:udemy_pengeluaran/widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
-  const Chart({Key key, this.recentTransactions}) : super(key: key);
+  Chart(this.recentTransactions);
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
-      double totalSum = 0 / 0;
+      double totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
@@ -22,23 +23,48 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E(weekDay));
-      print(totalSum);
+      print(DateFormat.E().format(weekDay).substring(0, 1));
+      print("adasad $totalSum");
 
       return {
-        "day": DateFormat.E(weekDay),
+        "day": DateFormat.E().format(weekDay).substring(0, 1),
         "amount": totalSum,
       };
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+      0.0,
+      (sum, item) {
+        return sum + item["amount"];
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("asd $groupedTransactionValues");
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: <Widget>[],
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
